3664e881702172496c745550b1cbecccd6fcb1e5
Enkhuliral Dechin <enkhee1117@gmail.com>
2022-09-20 20:06:03 -0500

    finished the first version

---
+++ src/Dog.java
@@ -0,0 +1,8 @@
+public class Dog {
+    // You will enter the dog's id, dog's name, weight and dog's age.
+    int id;
+    String name;
+    double weight;
+    int age;
+    
+}
--- src/DogManagement.java
+++ src/DogManagement.java
@@ -45,22 +45,126 @@ Program is submitted by the due date listed and pushed to assigned GitHub Reposi
     Author: C. Fulton
 */
 
+import java.util.Arrays;
 import java.util.Scanner; //Importing Scanner Class
 public class DogManagement {
     /*
      * Global Declaration for parallel arrays and Scanner Object
      */
     //DECLARING PARALEL ARRAYS OUTSIDE OF MAIN METHOD TO HOLD DOG DATA use the static keyword
-
+    public static int[] dogsAge = new int[12];
+    public static String[] dogsName = new String[12];
+    public static int[] dogsID = new int[12];
+    public static double[] dogsWeight = new double[12];
+    public static int dogCount = 0;
 
     //DECLARING SCANNER OBJECT
     static Scanner scn = new Scanner(System.in);
 
     public static void main(String[] args) throws Exception {
+        int menuOption = displayPrompt();
         
-        
-
+        while(menuOption != 4){
+            switch (menuOption) {
+                case 1:
+                // Create new Dog: 
+                    createDog();
+                    menuOption = displayPrompt();
+                    break;
+                case 2:
+                // Display Dog record:
+                    displayDog();
+                    menuOption = displayPrompt();
+                    break;
+                case 3:
+                // Update Dog report:
+                    updateDog();
+                    menuOption = displayPrompt();
+                    break;
+                default:
+                    System.out.println("Invalid menu option \n");
+                    menuOption = displayPrompt();
+            } //END OF SWITCH
+        } // END of WHILE
+        System.out.println("Program has ended!");
     }
+    public static void createDog(){
+        int id;
+        String name;
+        double weight;
+        int age;
+
+        System.out.println("You have selected to enter a new dog.");
+        System.out.print("Enter dog ID #: "); id = Integer.parseInt(scn.nextLine());
+        System.out.print("Enter dog Name: "); name = scn.nextLine();
+        System.out.print("Enter dog weight: "); weight = Double.parseDouble(scn.nextLine());
+        System.out.print("Enter dog age: "); age = Integer.parseInt(scn.nextLine());
+
+        // Validation
+
+        // Creation:
+        if (dogCount >= 12) return;
+        dogsAge[dogCount] = age;
+        dogsID[dogCount] = id;
+        dogsWeight[dogCount] = weight;
+        dogsName[dogCount] = name;
+        System.out.println("The following information has been entered:");
+        System.out.println("\t ID #: " + dogsID[dogCount]);
+        System.out.println("\t Name #: " + dogsName[dogCount]);
+        System.out.println("\t Weight #: " + dogsWeight[dogCount]);
+        System.out.println("\t Age #: " + dogsAge[dogCount]);
+        dogCount++;
+    }
+    public static void displayDog(){
+        for (int i = 0 ; i < dogCount ; i++ ){
+            System.out.println("ID #: " + dogsID[i] + " for " + dogsName[i]);
+        }
+        System.out.print("Please enter ID # to from above to display record:"); 
+        int displayId = Integer.parseInt(scn.nextLine());
+        int matchingIndex = -1;
+        for (int i = 0 ; i < dogCount ; i++){
+            if (dogsID[i] == displayId) matchingIndex = i;
+        }
+        if (matchingIndex == -1) {
+            System.out.println("Id # does not match dog id in system");
+            return;
+        } else {
+            System.out.println("\t ID #: " + dogsID[matchingIndex]);
+            System.out.println("\t Name #: " + dogsName[matchingIndex]);
+            System.out.println("\t Weight #: " + dogsWeight[matchingIndex]);
+            System.out.println("\t Age #: " + dogsAge[matchingIndex]);
+        }
+    }
+    public static void updateDog() {
+        System.out.println("Please enter the dog ID # to update record"); 
+        int updateDogID = Integer.parseInt(scn.nextLine());
+        int matchingIndex = -1;
+        for (int i = 0 ; i < dogCount ; i++){
+            if (dogsID[i] == updateDogID) matchingIndex = i;
+        }
+        if (matchingIndex == -1) {
+            System.out.println("Id # does not match dog id in system");
+            return;
+        } else {
+            // Get update info and update a dog. 
+            System.out.println("You have selected to update " + dogsName[matchingIndex]);
+            System.out.print("Enter dog ID #: "); int id = Integer.parseInt(scn.nextLine());
+            dogsID[matchingIndex] = id;
+            System.out.print("Enter dog Name: "); String name = scn.nextLine();
+            dogsName[matchingIndex] = name;
+            System.out.print("Enter dog weight: "); Double weight = Double.parseDouble(scn.nextLine());
+            dogsWeight[matchingIndex] = weight;
+            System.out.print("Enter dog age: "); int age = Integer.parseInt(scn.nextLine());
+            dogsAge[matchingIndex] = age;
+
+            System.out.println("\n The following information has been updated:");
+            System.out.println("\t ID #: " + dogsID[matchingIndex]);
+            System.out.println("\t Name #: " + dogsName[matchingIndex]);
+            System.out.println("\t Weight #: " + dogsWeight[matchingIndex]);
+            System.out.println("\t Age #: " + dogsAge[matchingIndex]);
+        }
+    }
+
 
     //Welcome method that outputs introductory text explaining program
     public static void welcome(){
